a661f201bdf79ce7b8c5c012af994f2c07b02a53
tymyevhenmpw <tymyevhen@gmail.com>
2025-03-13 09:27:40 +0100

    Initial commit

---
+++ constants/store.js
@@ -0,0 +1,12 @@
+export const Store = {
+  "name": "OTDmax",
+  "currency": "UAH",
+  "currency_sign": "₴",
+  "domain": "https://fo-scandinavia.vercel.app",
+  "database": "OTDmax_2025_03_13",
+  "default_image": {
+    "product_card": 0,
+    "product_page": 0,
+    "checkout_page": 0
+  }
+};
